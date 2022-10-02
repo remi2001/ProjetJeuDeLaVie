77f44c4e9e3b6e5f4e637adf5f40d04cc8e949ce
remi2001 <ometzremi54@gmail.com>
2022-10-02 11:48:01 +0200

    Création classe Terrain
    Création classe terrain + test dans le programme principale de cette classe + maj de la classe jeu

--- Program.cs
+++ Program.cs
@@ -8,6 +8,13 @@ namespace ProjetJeuDeLaVie
         {
             Jeu JeuDeLaVie = new Jeu();
             JeuDeLaVie.LancementDuJeu();
+
+            //Test de la classe Terrain
+            //Le pourcentage saisie doit etre entre 0 et 100
+            /*
+            Terrain Terrain_du_Jeu = new Terrain(0);
+            Terrain_du_Jeu.Affichage_du_terrain();
+            */
         }
     }
 }
